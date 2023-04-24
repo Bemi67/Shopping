7af3e8c2c333a0f6763db97585d757770734f557
Bemi67 <106610289+Bemi67@users.noreply.github.com>
2023-04-24 12:56:08 -0300

    Agregar archivos de proyecto.

---
+++ Shooping/Data/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shooping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        //Data notations validaciones
+        [Display(Name = "Country")]
+        [MaxLength(50, ErrorMessage = "{0}  must have maximum {1} characters.")]
+        [Required(ErrorMessage = "{0} is mandatory.")]
+        public string Name { get; set; }
+
+    }
+}
